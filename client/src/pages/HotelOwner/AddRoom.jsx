2d00b0c86b56f633e91a86e7ca2d2a32f3589c70
anushka <anushkasharma8418@gmail.com>
2026-01-11 13:33:09 +0530

    update router

--- client/src/pages/HotelOwner/AddRoom.jsx
+++ client/src/pages/HotelOwner/AddRoom.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import Title from '../../components/Title'
 import { assets } from '../../assets/assets'
+import { useAppContext } from '../../context/AppContext'
 
 const AddRoom = () => {
+   
+    const {axios, getToken} = useAppContext()
+
     const [images, setImages] = useState({
         1: null,
         2: null,
@@ -11,7 +15,7 @@ const AddRoom = () => {
     })
     const [inputs, setInputs] = useState({
         roomType: '',
-        pricePreNight: 0,
+        pricePerNight: 0,
         amenities:{
             'Free WiFi': false,
             'Free BreakFast': false,
@@ -20,9 +24,58 @@ const AddRoom = () => {
             'Pool Access': false,
         }
     })
+   
+      const [loading,  setLoading] = useState(false)
+
+
+   const onSubmitHandler  = async(e)=>{
+    e.preventdefault()
+    if(!inputs.roomType || !inputs.pricePerNight || !inputs.amenities || !Object.values(images).some(image=> image)){
+        toast.error("Please fill in all the details")
+        return;
+    }
+    setLoading(true);
+    try {
+        const formData = new FormData()
+        formData.append('roomType', inputs.roomType)
+        formData.append('pricePerNight', inputs.roompricePerNightType)
+
+        const amenities =  Object.keys(inputs.amenities).filter(key => inputs.amenities[key])
+        formData.append('amenities', JSON.stringify(amenities))
+
+        object.keys(images).forEach((key)=>{
+            images[key] && formData.append('images', images[key])
+        })
+
+       const {data} = await axios.post('/api/rooms/', formData, {headers:{Authorization: `Bearer ${await getToken()}`}})
+       if(data.success){
+        toast.success(data.message)
+        setInputs({
+            roomType: '',
+            pricePerNight: 0,
+            amenities: {
+                'Free WiFi': false,
+                'Free BreakFast': false,
+                'Room Service': false,
+                'Mountain view': false,
+                'Pool Access' : false
+            }
+        })
+        setImages({1: null, 2: null, 3: null,4: null})
+       }else{
+        toast.error(data.message)
+       }
+    } catch (error) {
+        toast.error(error.message)
+        
+    }finally{
+        setLoading(false);
+    }
+   }
+
 
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
         <Title align='left' font='outfit' title='Add Room' subTitle='Fill in the details carefully and accurate room details, pricing and amenities, to enhance the user booking experience.'/>
         {/* upload Area for Image */}
         <p className='text-gray-800 mt-10 '>Images</p>
@@ -64,8 +117,8 @@ const AddRoom = () => {
                 </div>
                ))}
            </div>
-           <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer '>
-            Add Room
+           <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer ' disabled={loading}>
+            {loading ? 'Adding...' : 'Add Room'}
            </button>
     </form>
   )
--- serverside/routes/userRouter.js
+++ serverside/routes/userRouter.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 import { getUserData, storeRecentSearchCities } from '../controller/userController.js';
-import { requireAuth } from '@clerk/express';
 
     const userRouter = express.Router();
 
     
-    userRouter.get('/get-user', requireAuth(), getUserData);
+    userRouter.get('/get-user', protect, getUserData);
     userRouter.post('/store-recent-search', protect, storeRecentSearchCities);
 
   export default userRouter;
